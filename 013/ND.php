9ea18402054adc5c21e560ff51d040396d57d73a
Simonas Kazlauskas <sksimkaz8@gmail.com>
2023-07-21 17:54:45 +0300

    function ND

---
+++ 013/ND.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// 1.
+
+function kazkas($name) {
+    echo "<h1>$name</h1>";
+}
+
+kazkas('hello world!');
+
+
+// 2.
+
+
+function darKazkas($text, $num) {
+    echo "<h$num>$text</h$num>";
+}
+
+
+darKazkas('sveikas', 2);
+
+// 3.
+
+// 4.
+
+function skaicius(int $num) {
+    do {
+
+    } while ($num % )
+    echo "";
+}
+
+skaicius(2);
+skaicius(6);
+skaicius(12);
+skaicius(36);
+skaicius(50);
+skaicius(10);
